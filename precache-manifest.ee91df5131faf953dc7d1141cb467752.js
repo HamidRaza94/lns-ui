3937c5467f3b2b35f58d6c50c78eacb81eb5e9af
HamidRazaST <hamid.raza@successive.tech>
2019-06-11 10:24:31 +0530

    Updates

---
+++ precache-manifest.ee91df5131faf953dc7d1141cb467752.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "142350cec04308646757c46367c525d5",
+    "url": "/react-client/index.html"
+  },
+  {
+    "revision": "5f38543ef2681961243e",
+    "url": "/react-client/static/js/2.482926c5.chunk.js"
+  },
+  {
+    "revision": "760e9a413d50fddd0b3a",
+    "url": "/react-client/static/js/main.df4e0679.chunk.js"
+  },
+  {
+    "revision": "4a24fb57e5e91233ecf3",
+    "url": "/react-client/static/js/runtime~main.1a624fa2.js"
+  }
+]);
