e71ae6a5888db2fed13c1cd18ce9435edffbb888
HamidRazaST <hamid.raza@successive.tech>
2019-07-07 14:08:44 +0530

    added

--- src/scenes/CentralSectionBoard/style.js
+++ src/scenes/CentralSectionBoard/style.js
@@ -2,9 +2,18 @@ const styles = (theme => ({
   root: {
     width: '100%',
   },
+
   heading: {
     fontSize: theme.typography.pxToRem(15),
     fontWeight: theme.typography.fontWeightRegular,
+  },
+
+  team: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    flexWrap: 'wrap',
+    padding: '20px 0px',
   },
 }));
 
