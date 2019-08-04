9a057854c56565b4f05ddad611def8342cfb61a6
HamidRazaST <hamid.raza@successive.tech>
2019-08-05 03:04:44 +0530

    added

--- src/scenes/NoMatch/NoMatch.jsx
+++ src/scenes/NoMatch/NoMatch.jsx
@@ -8,18 +8,18 @@ const NoMatch = props => {
 
   return (
     <div className={classes.root}>
-		  <div className={classes.notFound}>
-			  <div className={classes.notFound404}>
-				  <h3 className={classes.h3}>Oops! Page not found</h3>
-				  <h1 className={classes.h1}>
+      <div className={classes.notFound}>
+        <div className={classes.notFound404}>
+          <h3 className={classes.h3}>Oops! Page not found</h3>
+          <h1 className={classes.h1}>
             <span className={classes.span}>4</span>
             <span className={classes.span}>0</span>
             <span className={classes.span}>4</span>
           </h1>
-			  </div>
-			  <h2 className={classes.h2}>we are sorry, but the page you requested was not found</h2>
-		  </div>
-	  </div>
+        </div>
+        <h2 className={classes.h2}>we are sorry, but the page you requested was not found</h2>
+      </div>
+    </div>
   );
 };
 
