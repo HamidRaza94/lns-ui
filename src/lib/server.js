2b4bd2eada9cc3206447621a70311b1e3d2770f0
HamidRazaST <hamid.raza@successive.tech>
2019-08-15 00:24:30 +0530

    added

--- src/lib/server.js
+++ src/lib/server.js
@@ -6,17 +6,41 @@ const { SERVER_URL } = config;
 
 const connection = async (method, route, data) => (
   new Promise((resolve, reject) => {
-    axios({ method, url: `${SERVER_URL}/${route}`, data })
-    .then((res) => { resolve(res) })
-    .catch((error) => { reject({ error, message: 'Server Down', status: 404 }) });
+    axios({
+      method,
+      url: `${SERVER_URL}/${route}`,
+      data,
+    })
+    .then((res) => {
+      resolve(res);
+    })
+    .catch((error) => {
+      reject({
+        error,
+        message: 'Server Down',
+        status: 404,
+      });
+    });
   })
 );
 
 const secureConnection = async (method, route, data) => (
   new Promise((resolve, reject) => {
-    axios({ method, url: `${SERVER_URL}/${route}`, data })
-    .then((res) => { resolve(res) })
-    .catch((error) => { reject({ error, message: 'Server Down', status: 404 }) });
+    axios({
+      method,
+      url: `${SERVER_URL}/${route}`,
+      data,
+    })
+    .then((res) => {
+      resolve(res);
+    })
+    .catch((error) => {
+      reject({
+        error,
+        message: 'Server Down',
+        status: 404,
+      });
+    });
   })
 );
 
