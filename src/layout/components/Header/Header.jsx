1a03475a1e0850baa09e754ab76b1c0f3387113c
HamidRazaST <hamid.raza@successive.tech>
2019-08-17 23:19:15 +0530

    added

--- src/layout/components/Header/Header.jsx
+++ src/layout/components/Header/Header.jsx
@@ -1,50 +1,45 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import { Typography } from '@material-ui/core';
+import { withStyles, Typography } from '@material-ui/core';
 
 import styles from './style';
-import header from '../../../cms/header';
-import { LOGO_FOLDER, LOGO_IMAGE } from '../../../cms/constants';
+import { header } from '../../../cms';
+import { LOGO_FOLDER, LOGO_IMAGE } from '../../../lib/extra/constants';
 
-const Header = props => {
-  const { classes } = props;
-
-  return (
-    <div className={classes.root}>
-      <div className={classes.leftHeader}>
-        <img
-          src={LOGO_IMAGE}
-          alt={header.lns}
-          className={classes.logoImage}
-        />
-        <Typography align="center" className={classes.title}>
-          {header.lnsHindi} <br />
-          {header.pji} <br />
-          {header.approvedBy} <br />
-          {header.goi} <br />
-        </Typography>
-      </div>
-      <div className={classes.rightHeader}>
-        <img
-          src={`${LOGO_FOLDER}/gandhi-150.jpg`}
-          alt={header.gandhi150}
-          className={classes.gandhi150}
-        />
-        <img
-          src={`${LOGO_FOLDER}/swatch_india.png`}
-          alt={header.swatchBharat}
-          className={classes.swatchBharat}
-        />
-        <img
-          src={`${LOGO_FOLDER}/code_india.jpg`}
-          alt={header.codeIndia}
-          className={classes.codeIndia}
-        />
-      </div>
+const Header = ({ classes }) => (
+  <div className={classes.root}>
+    <div className={classes.leftHeader}>
+      <img
+        src={LOGO_IMAGE}
+        alt={header.lns}
+        className={classes.logoImage}
+      />
+      <Typography align="center" className={classes.title}>
+        {header.lnsHindi} <br />
+        {header.pji} <br />
+        {header.approvedBy} <br />
+        {header.goi} <br />
+      </Typography>
     </div>
-  );
-};
+    <div className={classes.rightHeader}>
+      <img
+        src={`${LOGO_FOLDER}/gandhi-150.jpg`}
+        alt={header.gandhi150}
+        className={classes.gandhi150}
+      />
+      <img
+        src={`${LOGO_FOLDER}/swatch_india.png`}
+        alt={header.swatchBharat}
+        className={classes.swatchBharat}
+      />
+      <img
+        src={`${LOGO_FOLDER}/code_india.jpg`}
+        alt={header.codeIndia}
+        className={classes.codeIndia}
+      />
+    </div>
+  </div>
+)
 
 Header.propTypes = {
   classes: PropTypes.object.isRequired,
