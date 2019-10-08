ca5589f01bd28c64715df762042d47c6bacc21c3
HamidRazaST <hamid.raza@successive.tech>
2019-10-08 17:46:25 +0530

    added

--- src/scenes/Grievance/component/SearchGrievance/SearchGrievance.jsx
+++ src/scenes/Grievance/component/SearchGrievance/SearchGrievance.jsx
@@ -4,6 +4,8 @@ import SearchIcon from '@material-ui/icons/Search';
 
 import style from '../FormForMember/style';
 import { DialogBox } from '../../../../components';
+import { connection } from '../../../../lib/server';
+import { API_METHOD, SERVER_ROUTE } from '../../../../lib/extra/constants';
 
 class SearchGrievance extends Component {
   constructor(props) {
@@ -28,7 +30,33 @@ class SearchGrievance extends Component {
   }
 
   handleFetchGrievance = () => {
+    const { grievanceId } = this.state;
+    const { snackBarStateUpdater } = this.props;
 
+    if (grievanceId) {
+      connection(API_METHOD.get, `${SERVER_ROUTE.grievance}/${grievanceId}`).then((res) => {
+        console.log('res is ', res);
+        this.setState({
+          IncidentDetail: res.data.data.placeOfIncident,
+          dateTimeIncident: res.data.data.dateTimeIncident,
+          summary: res.data.data.summary,
+          // attachment: res.data.data.attachment,
+        });
+        snackBarStateUpdater({
+          showSnackBar: true,
+          variant: 'success',
+          snackBarMsg: 'Successfully Fetch Grievance Data',
+        });
+      }).catch((err) => {
+        console.log('err is ', err);
+      })
+    } else {
+      snackBarStateUpdater({
+        showSnackBar: true,
+        variant: 'error',
+        snackBarMsg: 'Please Enter Grievance ID',
+      });
+    }
   }
 
   handleAgreeDialogBox = () => {
@@ -93,10 +121,10 @@ class SearchGrievance extends Component {
             </Fab>
           </div>
 
-          <Typography>Incident Detail: {IncidentDetail}</Typography>
-          <Typography>Date and Time of Incident: {dateTimeIncident}</Typography>
-          <Typography>Summary: {summary}</Typography>
-          <Typography>Attachment: {attachment}</Typography>
+          <Typography>Incident Detail: <b>{IncidentDetail}</b></Typography>
+          <Typography>Date and Time of Incident: <b>{dateTimeIncident}</b></Typography>
+          <Typography>Summary: <b>{summary}</b></Typography>
+          <Typography>Attachment: <b>{attachment}</b></Typography>
         </DialogBox>
       </>
     )
